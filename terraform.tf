60bf5ce028e734ec8f8da4838b30d8b39a786aef
Raman Shandilya <ramankrshandilya@gmail.com>
2023-07-23 20:04:15 +0530

    To be message

--- terraform.tf
+++ terraform.tf
@@ -5,11 +5,15 @@ terraform {
       version = "~> 5.0"
     }
   }
+    backend = {
+            region = "us-west-2"
+            bucket = "terraformtfstatebucket01"
+            key = "test/terraform/gitaction/ecr"
+
+    }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
-  bucket = "terraformtfstatebucket01"
-  key = "test/terraform/gitaction/ecr"
-}
+  }
