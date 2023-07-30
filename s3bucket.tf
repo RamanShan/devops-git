eda0ea5d3dc06f3c05d23689789b29dd0310548b
Raman Shandilya <ramankrshandilya@gmail.com>
2023-07-31 00:06:08 +0530

    backend config updated

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "foo" {
-  bucket = "terraformtfstatebucket01"
-  #bucket = var.s3bucketname
+  #bucket = "terraformtfstatebucket01"
+  bucket = var.s3bucketname
   acl    = "private"
   tags = {
     Name = "Terraform State Bucket"
