f6157c9312c695b96759257dd1028ac9a0ad5257
Raman Shandilya <ramankrshandilya@gmail.com>
2023-08-05 23:12:40 +0530

    s3bucket changes committed

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,6 +1,12 @@
 resource "aws_s3_bucket" "foo" {
   bucket = var.s3_bucketname
 
+  acl = "private"
+
+  tags = {
+    Name = "Terraform Bucket"
+  }
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -8,33 +14,4 @@ resource "aws_s3_bucket" "foo" {
       }
     }
   }
-
-  acl = "private"
-
-  tags = {
-    Name = "Terraform Bucket"
-  }
-}
-
-resource "aws_iam_policy" "bucket_policy" {
-  name        = "S3BucketPolicy"
-  description = "Policy for S3 bucket access"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowPublicRead"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = aws_s3_bucket.foo.arn
-        Condition = {
-          Bool = {
-            "aws:SecureTransport": "false"
-          }
-        }
-      }
-    ]
-  })
 }
