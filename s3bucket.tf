b6ddc6f3991ddada9e108df35e33551bfb7e8e50
RamanShan <86511068+RamanShan@users.noreply.github.com>
2024-06-27 10:41:54 +0530

    Update s3bucket_terraform.tf files

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,17 +1,17 @@
 resource "aws_s3_bucket_server_side_encryption_configuration" "foo" {
   bucket = var.s3_bucketname
 
-  acl = "private"
-
-  tags = {
-    Name = "Terraform Bucket"
-  }
-
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
     }
+  }
+
+  acl = "private"
+
+  tags = {
+    Name = "Terraform Bucket"
   }
 }
--- terraform.tf
+++ terraform.tf
@@ -1,22 +1,19 @@
 terraform {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-
   }
+  
   backend "s3" {
     bucket = "terraformtfstatebucket01"
-  required_providers {
-    #key    = "${var.s3_bucketname}/terraform/${var.s3_bucketname}"
-    #key    = "${var.s3_bucketname}/terraform/terraform.tfstate"
-    key     = "terraform/{{ var.s3_bucketname }}"
+    key    = "${var.s3_bucketname}/terraform/terraform.tfstate"
     region = "us-west-2"
   }
-    
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
-  }
+}
